d5345369ee0b20fd2d7c227e45dc7339d535e7f7
WillingNumberOne <willingnumbersone@gmail.com>
2017-05-24 04:22:21 -0500

    Initial state

---
+++ ClientApp/boot.ts
@@ -0,0 +1,17 @@
+import 'isomorphic-fetch';
+import { Aurelia, PLATFORM } from 'aurelia-framework';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap';
+import 'semantic/dist/semantic.min.css';
+import 'semantic/dist/semantic.min';
+declare const IS_DEV_BUILD: boolean; // The value is supplied by Webpack during the build
+
+export function configure(aurelia: Aurelia) {
+    aurelia.use.standardConfiguration();
+
+    if (IS_DEV_BUILD) {
+        aurelia.use.developmentLogging();
+    }
+
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app/components/app/app')));
+}
